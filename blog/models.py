d15704418c5b2c520718bfc329162b17a610d695
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-06-09 14:30:50 +0000

    Fix Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -11,4 +12,3 @@ class Post(models.Model):
 content = models.TextField()
 created_on = models.DateTimeField(auto_now_add=True)
 status = models.IntegerField(choices=STATUS, default=0)
-STATUS = ((0, "Draft"), (1, "Published"))
