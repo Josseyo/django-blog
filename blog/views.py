15a57a1d663168569cf9877038b6e6936b9307c0
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-06-19 14:48:43 +0000

    fix blog view

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "post_list.html"
