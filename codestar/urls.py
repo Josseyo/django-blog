085fb2fcbfc43dafd004d3f9e2a0ecc8fede972c
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-06-21 11:13:45 +0000

    add URL pattern to serve the static files

--- codestar/urls.py
+++ codestar/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +26,19 @@ urlpatterns = [
     path("", include("blog.urls"), name='blog-urls'),
    
 ]
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name='blog-urls'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
