8e8ef7cdd92113a6c74cd1c83b345005c704c3da
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-06-26 12:14:58 +0000

    Add comment form

--- blog/models.py
+++ blog/models.py
@@ -19,19 +19,20 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-            ordering = ["-created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"The title of this post is {self.title}"
-                
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-josseyo-djangoblog-wh14imzy6ov.ws.codeinstitute-ide.net",
-    "blogga-1c74101097cb.herokuapp.com"]
+    "blogga-1c74101097cb.herokuapp.com",
+    'codeanyapp.com', '.herokuapp.com']
 
 # Application definition
 
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django_summernote',
     'blog',
     'about',
@@ -54,6 +57,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
