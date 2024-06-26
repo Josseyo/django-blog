afb08a3c4f5c044a6a5a11b89ddbd43b1182120a
Josefin Yoshida Dahlqvist <josefin@yomaco.com>
2024-06-26 16:30:30 +0000

    Add handle form POST request

--- about/views.py
+++ about/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
 
 def about_me(request):
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
     """
     Renders the About page
     """
